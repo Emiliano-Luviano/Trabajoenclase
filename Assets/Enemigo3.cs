9e053d629f9b0c0cc4336c7138f0a537eec25694
Emiliano-Luviano <jeluviano@cocoschool.mx>
2023-11-23 14:52:32 -0600

    Enemigo Tanque

--- Assets/Enemigo3.cs
+++ Assets/Enemigo3.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Properties;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MovimientoEnemigo : MonoBehaviour
+public class Enemigo3 : MonoBehaviour
 {
     public float speed;
     public TextoPuntaje textObject;
     public TextoVidas textObjectVidas;
+    public int vidas;
     void Start()
     {
         textObject = FindAnyObjectByType<TextoPuntaje>();
         textObjectVidas = FindAnyObjectByType<TextoVidas>();
     }
 
-  
+    
     void Update()
     {
         transform.Translate(Vector3.left.normalized * speed * Time.deltaTime);
@@ -22,38 +24,36 @@ public class MovimientoEnemigo : MonoBehaviour
 
     private void OnTriggerEnter(Collider collision)
     {
-        
-
-
         if (collision.gameObject.tag == "Pared")
         {
-
+            Destroy(gameObject);
         }
-        else if (collision.gameObject.tag =="Jugador")
+        else if (collision.gameObject.tag == "Jugador")
         {
             textObjectVidas.puntosVida = textObjectVidas.puntosVida - 1;
-            if (textObjectVidas.puntosVida==0)
+            if (textObjectVidas.puntosVida == 0)
             {
                 Destroy(collision.gameObject);
-                SceneManager.LoadScene(3);               
+                SceneManager.LoadScene(3);
             }
             Destroy(gameObject);
 
         }
         else
         {
+            vidas = vidas - 1;
+            if (vidas == 0)
+            {
+                Destroy(gameObject);
+            }
             textObject.puntos = textObject.puntos + 1;
             Destroy(collision.gameObject);
-            Destroy(gameObject);
-            if(textObject.puntos==10)
+            
+            if (textObject.puntos == 10)
             {
                 SceneManager.LoadScene(2);
             }
         }
-
-        
-
-        
 
     }
 }
