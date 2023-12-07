c3ddff3ac83a1dca65ca4205aa437c921adb9199
Emiliano-Luviano <jeluviano@cocoschool.mx>
2023-12-07 13:44:28 -0600

    final?

--- Assets/Enemigo.cs
+++ Assets/Enemigo.cs
@@ -5,24 +5,48 @@ using UnityEngine;
 
 public class Enemigo : MonoBehaviour
 {
+
     public GameObject enemy;
+    public GameObject tanque;
+    public GameObject veloz;
+    public GameObject zigzag;
     public float max;
     public float min;
-
     public float timer;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         timer = timer - Time.deltaTime;
+
+        int enemigoElegido = Random.Range(0, 100);
         if (timer <= 0)
         {
             float posY = Random.Range(min, max);
-            Instantiate(enemy, new Vector3(transform.position.x, posY, 0), Quaternion.identity);
+            if (enemigoElegido < 25)
+            {
+                Instantiate(enemy, new Vector3(transform.position.x, posY, 0), Quaternion.identity);
+                timer = 1;
+            }
+
+            else if (enemigoElegido < 50)
+            {
+                Instantiate(tanque, new Vector3(transform.position.x, posY, 0), Quaternion.identity);
+            }
+            else if (enemigoElegido < 75)
+            {
+                Instantiate(veloz, new Vector3(transform.position.x, posY, 0), Quaternion.identity);
+            }
+            else if (enemigoElegido < 100)
+            {
+                Instantiate(zigzag, new Vector3(transform.position.x, posY, 0), Quaternion.identity);
+            }
             timer = 1;
+
         }
     }
 }
+
--- Assets/Enemigo4.cs
+++ Assets/Enemigo4.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemigo4 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public TextoPuntaje textObject;
+    public TextoVidas textObjectVidas;
+
     void Start()
     {
-        
+        textObject = FindAnyObjectByType<TextoPuntaje>();
+        textObjectVidas = FindAnyObjectByType<TextoVidas>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        transform.Translate(Vector3.left.normalized * speed *speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+
+
+
+        if (collision.gameObject.tag == "Pared")
+        {
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag == "Jugador")
+        {
+            textObjectVidas.puntosVida = textObjectVidas.puntosVida - 1;
+            if (textObjectVidas.puntosVida == 0)
+            {
+                Destroy(collision.gameObject);
+                SceneManager.LoadScene(3);
+            }
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            textObject.puntos = textObject.puntos + 1;
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            if (textObject.puntos == 10)
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
     }
 }
